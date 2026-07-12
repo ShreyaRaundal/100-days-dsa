777808306297784646578fca25bff337751fabf5
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2026-07-12 17:17:52 +0530

    Add files via upload

---
+++ Day_02_Array/AccessEle.java
@@ -0,0 +1,11 @@
+package Day_02_Array;
+
+class AccessEle{
+    public static void main(String[] args){
+        int [] arr={10,20,30,40,50,60};
+        System.out.println(arr[0]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+    }
+}
+
