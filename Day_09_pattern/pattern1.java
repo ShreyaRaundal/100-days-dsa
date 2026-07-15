47ffe022f0131481e13c0f62e562d3a18f4d92e3
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2026-07-15 13:23:09 +0530

    Add files via upload

---
+++ Day_09_pattern/pattern1.java
@@ -0,0 +1,18 @@
+package Day_09_pattern;
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+public class pattern1 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0;1<n;i++){
+            for(int j=0;1<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
